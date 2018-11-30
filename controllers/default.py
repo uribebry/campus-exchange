3bb5a32040a56ef05a41d50bdb86ac7146a4bfc6
bfabello <brianfabello@gmail.com>
2018-11-30 02:23:31 -0800

    category feed working

--- controllers/default.py
+++ controllers/default.py
@@ -36,16 +36,6 @@ def user_bar():
     return bar
 
 
-def add():
-    """Adds a checklist for the"""
-    form = SQLFORM(db.checklist)
-    if form.process().accepted:
-        session.flash = T("Checklist added.")
-        redirect(URL('default','index'))
-    elif form.errors:
-        session.flash = T('Please correct the info')
-    return dict(form=form)
-
 def user():
     """
     exposes:
@@ -65,6 +55,7 @@ def user():
     return dict(form=auth())
 
 
+@auth.requires_login()
 @cache.action()
 def download():
     """
@@ -82,5 +73,157 @@ def call():
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
     return service()
+
+@auth.requires_login()
+def add():
+    #Function to add a listing
+    grid = SQLFORM(db.listing)
+    if grid.process().accepted:
+        session.flash = T('Post Added')
+        redirect(URL('default', 'posting'))
+    elif grid.errors:
+        session.flash = T('Please correct the info')
+    export_classes = dict(csv=True, json=False, html=False,
+    tsv=False, xml=False, csv_with_hidden_cols=False,
+    tsv_with_hidden_cols=False)
+    return dict(grid=grid)
+
+@auth.requires_login()    
+def view():
+    # Function to view a listing
+    p = db.listing(request.args(0)) or redirect(URL('default', 'posting'))
+    grid = SQLFORM(db.listing, record = p, readonly = True)
+    button = A('return to listings', _class='btn btn-default', _href=URL('default', 'posting'))
+    export_classes = dict(csv=True, json=False, html=False,
+    tsv=False, xml=False, csv_with_hidden_cols=False,
+    tsv_with_hidden_cols=False)
+    return dict(p=p, button = button)
+
+@auth.requires_login()
+def edit():
+    #Function to edit listings
+    p = db.listing(request.args(0)) or redirect(URL('default', 'posting'))
+    if p.user_id != auth.user_id:
+        session.flash = T('You are not authorized!')
+        redirect(URL('default', 'posting'))
+    grid = SQLFORM(db.listing, record=p)
+    if grid.process().accepted:
+        session.flash = T('updated')
+        redirect(URL('default', 'add', args=[p.id]))
+    export_classes = dict(csv=True, json=False, html=False,
+    tsv=False, xml=False, csv_with_hidden_cols=False,
+    tsv_with_hidden_cols=False)
+    return dict(grid=grid)
+
+@auth.requires_login()
+@auth.requires_signature()
+def delete():
+    #Function to delete listings
+    p = db.listing(request.args(0)) or redirect(URL('default', 'posting'))
+    if p.user_id != auth.user_id:
+        session.flash = T('You are not authorized!')
+        redirect(URL('default', 'posting'))
+    confirm = FORM.confirm('delete listing')
+    grid = SQLFORM(db.listing, record = p, readonly = True, upload = URL('download'))
+    if confirm.accepted:
+        db(db.listing.id == p.id).delete()
+        session.flash = T('listing is deleted')
+        redirect(URL('default', 'posting'))
+    export_classes = dict(csv=True, json=False, html=False,
+    tsv=False, xml=False, csv_with_hidden_cols=False,
+    tsv_with_hidden_cols=False)
+    return dict(p=p, grid=grid, confirm=confirm)
+
+@auth.requires_login()
+@auth.requires_signature()
+def soldCheck():
+    #an item to show the user the avalibility of a product
+    item = db.listing(request.args(0)) or redirect(URL('default', 'posting'))
+    item.update_record(sold = not item.sold) 
+    redirect(URL('default', 'posting'))
+
+def posting():
+    #the posting to show the grid
+    show_all = request.args(0) == 'all'
+    q = (db.listing) if show_all else (db.listing.sold == False)
+    export_classes = dict(csv=True, json=False, html=False,
+         tsv=False, xml=False, csv_with_hidden_cols=False,
+         tsv_with_hidden_cols=False)
+
+    def deleteButton(row):
+        b = ''
+        if auth.user_id == row.user_id:
+            b = A('Delete', _class='btn btn-info', _href=URL('default', 'delete', args=[row.id], user_signature=True))
+        return b
+
+    def editButton(row):
+        b = ''
+        if auth.user_id == row.user_id:
+            b = A('Edit', _class='btn btn-info', _href=URL('default', 'edit', args=[row.id]))
+        return b
+
+    def soldButton(row):
+        b = ''
+        if auth.user_id == row.user_id:
+            b = A('Sold/Not Sold', _class='btn btn-info', _href=URL('default', 'soldCheck', args=[row.id], user_signature=True))
+        return b
+
+    def viewButton(row):
+        b = A('View', _class='btn btn-info', _href=URL('default','view',args=[row.id]))
+        return b
+
+    def profileButton(row):
+        b = A('Profile', _class='btn btn-info', _href=URL('default','profile',args=[row.user_id]))
+        return b
+    
+
+# # shortens the length of the descriptions on posting.html
+#     def description(row):
+#         return row.description[:40]
+
+    # all the buttons for posting.html
+    links = [
+        dict(header='', body = viewButton),
+        dict(header='', body = profileButton),
+        dict(header='', body = deleteButton),
+        dict(header='', body = editButton),
+        dict(header='', body = soldButton),
+        ]
+
+    # if len(request.args) == 0:
+    #     links.append(dict(header='Description', body = shorterL))
+    #     db.listing.description.readable = False
+
+    start_idx = 1 if show_all else 0
+    export_classes = dict(csv=True, json=False, html=False,
+    tsv=False, xml=False, csv_with_hidden_cols=False,
+    tsv_with_hidden_cols=False)
+
+# declear the grid once
+    grid = SQLFORM.grid(q,
+        args=request.args[:start_idx],
+        fields=[db.listing.sold,
+                db.listing.seller,
+                db.listing.item,
+                db.listing.price,
+                db.listing.user_id,
+                # db.listing.date_posted
+                ],
+        links=links,
+        editable=False,
+        deletable=False,
+        csv=False,
+        details=False,
+        )
+
+    add = A('Add Post', _class='btn btn-default', _href=URL('default', 'add'))
+
+# to show all or only avalible items
+    if show_all:
+        button = A('See only avalible listing', _class='btn btn-default', _href=URL('default', 'posting'))
+    else:
+        button = A('See all listing', _class='btn btn-default', _href=URL('default', 'posting', args=['all']))
+
+    return dict(grid=grid, button=button, add=add)
 
 
--- models/tables.py
+++ models/tables.py
@@ -12,6 +12,14 @@ import datetime
 def get_user_email():
     return auth.user.email if auth.user is not None else None
 
+def get_date():
+    return request.now if auth.user is not None else None
+
+def get_name():
+    return auth.user.first_name if auth.user is not None else None
+
+def get_user_id():
+    return auth.user.id if auth.user is not None else None
 
 db.define_table('checklist',
                 Field('user_email', default=get_user_email()),
@@ -20,13 +28,33 @@ db.define_table('checklist',
                 Field('updated_on', 'datetime', update=datetime.datetime.utcnow()),
                 Field('is_public', 'boolean', default=False))
 
+db.define_table('listing',
+                Field('item'),
+                Field('price', 'decimal(6,2)'),
+                Field('sold', 'boolean', default=False),
+                Field('image', 'upload'),
+                Field('seller', default=get_name()),
+                Field('user_id', default=get_user_id()),
+                Field('phone'),
+                Field('email', default=get_user_email()),
+                Field('likes', 'integer', default=0),
+                Field('description', 'text'),
+                # Field('date_posted', 'date', default=get_date())
+                )
+
 db.checklist.user_email.writable = False
 db.checklist.user_email.readable = False
 db.checklist.updated_on.writable = db.checklist.updated_on.readable = False
 db.checklist.id.writable = db.checklist.id.readable = False
-
 # Hides the check box 'is_public' for the user when creating a memo
 db.checklist.is_public.readable = db.checklist.is_public.writable = False
+
+db.listing.likes.writable = False
+db.listing.user_id.writable = db.listing.user_id.readable = False
+db.listing.seller.writable = False
+db.listing.phone.writable = db.listing.phone.readable = False
+# db.listing.date_posted.writable = False
+db.listing.email.writable = db.listing.email.readable = False
 
 # after defining tables, uncomment below to enable auditing
 # auth.enable_record_versioning(db)
